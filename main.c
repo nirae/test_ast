d6141eb334b646748a6f22f8a52b2ad5e12a01ed
ndubouil <ndubouil@student.42.fr>
2019-01-10 22:27:22 +0100

    automate ok pour calculatrice

--- main.c
+++ main.c
@@ -1,28 +1,51 @@
 #include "libft.h"
 #include "libftprintf.h"
 
-#define START_STATE 0
-#define CHAR_STATE 1
-#define	IGNORE_STATE_SPACE 2
-#define	IGNORE_STATE_DOTCOMMA 3
-#define	SPACE_STATE 4
-#define	DOTCOMMA_STATE_STAR 5
-#define	DOTCOMMA_STATE 6
-#define	NONE_STATE 7
+#define TRUE						1
+#define FALSE						0
 
-#define	CHAR_INDEX 0
-#define	DOTCOMMA_INDEX 1
-#define	SPACE_INDEX	2
+#define START_STATE					0
+#define NUMBER_STATE				1
+#define	OPEN_BRACKET_STATE			2
+#define	CLOSE_BRACKET_STATE			3
+#define	STAR_CLOSE_BRACKET_STATE	4
+#define	PLUS_STATE					5
+#define	STAR_PLUS_STATE				6
+#define	MINUS_STATE					7
+#define STAR_MINUS_STATE			8
+#define MULTIPLY_STATE				9
+#define STAR_MULTIPLY_STATE			10
+#define DIVISION_STATE				11
+#define STAR_DIVISION_STATE			12
+#define STAR_NUMBER_AOB_STATE			13
+#define NONE_STATE					14
 
-int get_index_from_char(char c)
+#define	NUMBER_INDEX				0
+#define	OPEN_BRACKET_INDEX			1
+#define	CLOSE_BRACKET_INDEX			2
+#define PLUS_INDEX					3
+#define MINUS_INDEX					4
+#define MULTIPLY_INDEX				5
+#define DIVISION_INDEX				6
+#define	NONE_INDEX					7
+
+int get_index_from_char(char *s, int i)
 {
-	if (c >= 97 && c <= 122)
-		return (CHAR_INDEX);
-	else if (c == ';')
-		return (DOTCOMMA_INDEX);
-	else if (c == ' ')
-		return (SPACE_INDEX);
-	return (NONE_STATE);
+	if (s[i] >= 48 && s[i] <= 57)
+		return (NUMBER_INDEX);
+	else if (s[i] == '(' && s[i + 1])
+		return (OPEN_BRACKET_INDEX);
+	else if (s[i] == ')')
+		return (CLOSE_BRACKET_INDEX);
+	else if (s[i] == '+' && s[i + 1])
+		return (CLOSE_BRACKET_INDEX);
+	else if (s[i] == '-' && s[i + 1])
+		return (CLOSE_BRACKET_INDEX);
+	else if (s[i] == 'x' && s[i + 1])
+		return (CLOSE_BRACKET_INDEX);
+	else if (s[i] == '/' && s[i + 1])
+		return (CLOSE_BRACKET_INDEX);
+	return (NONE_INDEX);
 }
 
 void put_char_in_stack(char (*stack)[512], char c)
@@ -32,16 +55,36 @@ void put_char_in_stack(char (*stack)[512], char c)
 	(*stack)[ft_strlen(*stack)] = c;
 }
 
+int	is_acceptor(int state)
+{
+	if (state == CLOSE_BRACKET_STATE || state == PLUS_STATE
+		|| state == MINUS_STATE || state == MULTIPLY_STATE
+		|| state == DIVISION_STATE  || state == STAR_PLUS_STATE
+		|| state == STAR_CLOSE_BRACKET_STATE || state == STAR_MINUS_STATE
+		|| state == STAR_MULTIPLY_STATE || state == STAR_DIVISION_STATE
+		|| state == STAR_NUMBER_AOB_STATE)
+		return (TRUE);
+	return (FALSE);
+}
+
+int is_star(int state)
+{
+	if (state == STAR_CLOSE_BRACKET_STATE || state == STAR_PLUS_STATE
+		|| state == STAR_MINUS_STATE || state == STAR_MULTIPLY_STATE
+		|| state == STAR_DIVISION_STATE || state == STAR_NUMBER_AOB_STATE)
+		return (TRUE);
+	return (FALSE);
+}
+
 void lexer(char *line, char ***tokens)
 {
 	int i;
 	int j;
 	char stack[512];
-	static int A[4][4] = {
-		{CHAR_STATE, DOTCOMMA_STATE, IGNORE_STATE_SPACE},
-		{CHAR_STATE, DOTCOMMA_STATE_STAR, SPACE_STATE},
-		{CHAR_STATE, DOTCOMMA_STATE, IGNORE_STATE_SPACE},
-		{CHAR_STATE, IGNORE_STATE_DOTCOMMA, IGNORE_STATE_SPACE}
+	static int A[4][9] = {
+		{NUMBER_STATE, OPEN_BRACKET_STATE, CLOSE_BRACKET_STATE, PLUS_STATE, MINUS_STATE, MULTIPLY_STATE, DIVISION_STATE, NONE_STATE},
+		{NUMBER_STATE, NONE_STATE, STAR_CLOSE_BRACKET_STATE, STAR_PLUS_STATE, STAR_MINUS_STATE, STAR_MULTIPLY_STATE, STAR_DIVISION_STATE, NONE_STATE},
+		{STAR_NUMBER_AOB_STATE, NONE_STATE, NONE_STATE, NONE_STATE, NONE_STATE, NONE_STATE, NONE_STATE, NONE_STATE}
 	};
 	int current_state;
 	int next_state;
@@ -53,22 +96,21 @@ void lexer(char *line, char ***tokens)
 	i = -1;
 	while (line[++i])
 	{
-		// ft_printf("index from char = %d\n", get_index_from_char(line[i]));
+		//ft_printf("index from char = %d\n", get_index_from_char(line[i]));
 		// ft_printf("char = %c, next state = %d\n", line[i], next_state);
-		next_state = A[current_state][get_index_from_char(line[i])];
+		next_state = A[current_state][get_index_from_char(line, i)];
 		ft_printf("i[%d], char[%c] current state[%d] next_state[%d]\n", i, line[i], current_state, next_state);
-		if (next_state != IGNORE_STATE_DOTCOMMA && next_state != IGNORE_STATE_SPACE)
-			put_char_in_stack(&stack, line[i]);
+		put_char_in_stack(&stack, line[i]);
 		if (next_state == NONE_STATE)
 		{
-			ft_printf("erreur\n");
+			ft_printf("Syntax error near character number %d\n", i + 1);
 			exit(0);
 		}
 		// ETAT ACCEPTEUR
-		else if (next_state == SPACE_STATE || next_state == DOTCOMMA_STATE_STAR || next_state == DOTCOMMA_STATE)
+		else if (is_acceptor(next_state))
 		{
 			// ETAT ETOILE
-			if (next_state == DOTCOMMA_STATE_STAR || next_state == SPACE_STATE)
+			if (is_star(next_state))
 			{
 				stack[ft_strlen(stack) - 1] = 0;
 				i--;
@@ -82,6 +124,9 @@ void lexer(char *line, char ***tokens)
 		}
 		current_state = next_state;
 	}
+	if (current_state == NUMBER_STATE)
+		(*tokens)[j] = ft_strdup(stack);
+	// ft_printf("current state after while = %d\n", current_state);
 }
 
 int main(int ac, char **av)
